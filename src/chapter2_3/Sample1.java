af578368c275305f93d8627cbd6cc0de3dd7395f
ruisunene <114806430+ruisunene@users.noreply.github.com>
2023-02-03 13:36:09 +0900

    chapter2_3

---
+++ src/chapter2_3/Sample1.java
@@ -0,0 +1,13 @@
+package chapter2_3;
+
+public class Sample1 {
+	public static void main(String[] srgs) {
+		
+		int number = 100;
+		System.out.println(number);
+		
+		number = 200;
+		int times = number + 10;
+		System.out.println(times);
+	}
+}
