c9011fa921479f3228443388840536f049b913a3
ruisunene <114806430+ruisunene@users.noreply.github.com>
2023-02-03 14:34:01 +0900

    2.5

---
+++ src/chapter2_5/Sampie1.java
@@ -0,0 +1,13 @@
+package chapter2_5;
+
+public class Sampie1 {
+	public static void main(String[] args) {
+		
+		String str = """
+				おはよう
+				こんばんわ
+				こんにちわ
+				""";
+				System.out.println(str);	
+}
+}
---
+++ src/exercise/E251_2.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class E251_2 {
+	public static void main(String[] args) {
+		
+		System.out.println(1000 + "\t田中宏");
+		System.out.println(1010 + "\t谷川太郎");
+		System.out.println(1020 + "\t鈴木誠");
+	}
+}
